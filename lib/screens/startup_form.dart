2943c05e212616a9c13dc332e49aba6737cc72cf
Abass Mutala <abassmutala@gmail.com>
2022-01-27 17:09:35 +0000

    update

--- lib/screens/startup_form.dart
+++ lib/screens/startup_form.dart
@@ -68,7 +68,7 @@ class _StartUpFormState extends State<StartUpForm> {
                     height: 15,
                   ),
                   Text(
-                    "Welcome lets work togetherssd  dsd,",
+                    "Welcome to ghana,",
                     style: TextStyle(
                         fontSize: large
                             ? 40
